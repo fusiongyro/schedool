8c0c2c02bb132823ccf0856a5c0c64a2e5172df4
Daniel Lyons <fusion@storytotell.org>
2010-01-25 14:41:12 -0700

    query language parser, first try, very broken. doesn't understand spaces, doesn't handle parentheses, doesn't canonicalize strings, but does handle some of the operations.

--- src/Schedool/Query.hs
+++ src/Schedool/Query.hs
@@ -1,3 +1,35 @@
-module Query (runQuery) where
+module Schedool.Query (runQuery) where
+
+import Schedool.Section
+
+import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Char
+import Text.ParserCombinators.Parsec.Expr
     
+data ClassName = Class String Integer 
+                 deriving (Show, Eq, Read)
+
 runQuery  :: [Section] -> String -> [Section]
+runQuery = undefined
+
+-- | Parses a string of the form "CS 121" and returns the
+--   corresponding data structure: Class "CS" 121.
+parseClass :: CharParser st Expr
+parseClass = do
+  dept <- many1 letter
+  spaces
+  num <- many1 digit
+  return $ Value (Class dept (read num))
+
+data Expr = Expr `OrExp` Expr
+          | Expr `AndExp` Expr
+          | Expr `AndOrExp` Expr
+          | Value ClassName
+            deriving (Show, Eq, Read)
+
+table = [[Infix (string "," >> return AndExp) AssocLeft,
+          Infix (string "and" >> return AndExp) AssocLeft],
+         [Infix (string "or" >> return OrExp) AssocLeft],
+         [Infix (string "and/or" >> return AndOrExp) AssocLeft]]
+
+exprParser = buildExpressionParser table parseClass
